ca6ca0fb4b21df526f7415dd6b13a11863f8219c
Unnatti <70205409+Unnatti@users.noreply.github.com>
2021-01-09 20:22:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var man, man_img;
+
+
+function preload(){
+    man.loadImage("images/Walking Frame/walking_1.png");
+}
+
+function setup(){
+    canvas = createCanvas(300, 200);
+    backgroung.shapeColor("black");  
+
+    
+
+man = createSprite(100, 120, 10, 10);
+man.Img("walking_1.png")
+
+}
+
+function draw(){
+   
+}   
+
